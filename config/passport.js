443a22a3035a8733d1cc433c114afd71a58e4ed5
GIRIJESHKUSHWAHA <girijeshkushwaha786@gmail.com>
2022-06-15 12:02:49 +0530

    update

--- config/passport.js
+++ config/passport.js
@@ -16,6 +16,7 @@ module.exports = function (passport) {
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
           googleId: profile.id,
+          email: profile.emails[0].value,
           name: profile.displayName,
           image: profile.photos[0].value,
         };
